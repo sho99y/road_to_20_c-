d009a35da091bb227bb710850896500fbb8833c8
sho99y <pises99@gmail.com>
2019-10-22 17:34:43 +0300

    почти готовая

---
+++ PhantomPainv2.1/interpolyatsia/Form1.cs
@@ -0,0 +1,171 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace interpolyatsia
+{
+    public partial class Form1 : Form
+    {
+        GussMeth gaus;
+
+        //переменные используемые в классе
+        static double pluss = 0.01;
+        static double minus = 0.01;
+        double finish = 0;
+
+        public void draw_chart(int i, double x, double y) //нарисовать линию 
+        {
+            chart1.Series[i].Points.AddXY(x, y);
+        }
+
+        public void draw_gaus_chart() //нарисовать гауса и E 
+        {
+            for (double i = data.get_x_min; i <= data.get_x_max; i += 0.01)
+            {
+                draw_chart(2, i, gaus.value_y_gauss(i));
+            }
+
+            for (double i = data.get_x_min; i <= data.get_x_max; i += 0.01)
+            {
+                chart2.Series[0].Points.AddXY(i, Math.Abs(gaus.value_y_gauss(i) - data.y_equation(i)));
+            }
+        }
+
+        public void draw_gaus_chart(double second_value) //нарисовать гауса и E но можно еще и размер поменять 
+        {
+            for (double i = data.get_x_min; i <= second_value; i += 0.01)
+            {
+                draw_chart(2, i, gaus.value_y_gauss(i));
+            }
+
+            for (double i = data.get_x_min; i <= second_value; i += 0.01)
+            {
+                chart2.Series[0].Points.AddXY(i, Math.Abs(gaus.value_y_gauss(i) - data.y_equation(i)));
+            }
+        }
+
+        public void draw_y_equation() //нарисовать данную в задании функцию 
+        {
+            for (double i = data.get_x_min; i <= data.get_x_max; i += 0.01)
+            {
+                draw_chart(0, i, data.y_equation(i));
+            }
+        }
+
+        public void draw_y_equation(double second_value) //нарисовать данную в задании функцию с возможность изменить размер 
+        {
+            for (double i = data.get_x_min; i <= second_value; i += 0.01)
+            {
+                draw_chart(0, i, data.y_equation(i));
+            }
+        }
+
+        public void draw_x_dots() //нарисовать точки 
+        {
+            for (int i = 0; i <= data.x_value.Length - 1; i++)
+            {
+                draw_chart(1, data.x_value[i], data.y_equation(data.x_value[i]));
+            }
+        }
+
+        public void set_x_value() //получить значения x из текстового поля 
+        {
+            String[] st_x_value = textBox1.Text.Split(' ');
+            data.x_value = new double[st_x_value.Length];
+            data.a_value_gauss = new double[st_x_value.Length];
+
+            for (int i = 0; i <= st_x_value.Length - 1; i++)
+            {
+                data.x_value[i] = Convert.ToDouble(st_x_value[i]);
+            }
+            finish = data.get_x_max;
+        }
+
+        //  Граница
+
+        public Form1() //когда запускается программа 
+        {
+            InitializeComponent();
+            set_x_value();
+
+            draw_y_equation();
+            draw_x_dots();
+        }
+
+        private void button1_Click(object sender, EventArgs e) //интерполяция по гауссу 
+        {
+            chart1.Series[2].Points.Clear();
+            chart2.Series[0].Points.Clear();
+
+            set_x_value();  
+
+            gaus = new GussMeth();
+
+            draw_gaus_chart();
+        }
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e) //убрать, добавить первую линию, но её нужно доделать!!!!!!!
+        {
+            if (checkBox1.Checked == false)
+            {
+                chart1.Series[0].Points.Clear();
+            }
+            else
+            {
+                chart1.Series[0].Points.Clear();
+                for (double i = data.get_x_min; i <= finish; i += 0.01)
+                {
+                    draw_chart(0, i, data.y_equation(i));
+                }
+            }
+        }
+
+        private void restart_Click(object sender, EventArgs e) //перерисовать точки 
+        {
+            chart1.Series[0].Points.Clear();
+            chart1.Series[1].Points.Clear();
+            set_x_value();
+
+            draw_y_equation();
+            draw_x_dots();
+        }
+
+        private void button_pluss_Click(object sender, EventArgs e) //увеличить размер линий 
+        {
+            
+            chart1.Series[0].Points.Clear();
+            chart1.Series[2].Points.Clear();
+            chart2.Series[0].Points.Clear();
+
+            gaus = new GussMeth();
+
+            draw_y_equation(finish + pluss);
+            draw_gaus_chart(finish + pluss);
+
+            finish += pluss;
+            pluss += 0.01;
+        }
+
+        private void button_minus_Click(object sender, EventArgs e) //уменьшить размер линий 
+        {
+            
+            chart1.Series[0].Points.Clear();
+            chart1.Series[2].Points.Clear();
+            chart2.Series[0].Points.Clear();
+
+            gaus = new GussMeth();
+
+            draw_y_equation(finish - minus);
+            draw_gaus_chart(finish - minus);
+
+            finish -= minus;
+            minus += 0.01;
+        }
+    }
+}
---
+++ PhantomPainv2.1/interpolyatsia/GussMeth.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace interpolyatsia
+{
+    class GussMeth
+    {
+        private double[] graph_gauss() //оно работает значит лучше его не трогать!!!!! 
+        {
+            int n = data.x_value.Length;
+            double s = 0;
+            double[,] a = new double[n, n];
+            double[] y = new double[n];
+            double[] x = new double[n];
+           
+            for (int i = 0; i <n; i++)
+            {
+                x[i] = 0;
+            }
+
+            for (int i = 0; i < n; i++)
+                y[i] = data.y_equation(data.x_value[i]);
+
+            for (int k = 0; k < n; k++)
+            {
+                for (int i = 0 ; i < n; i++)
+                {
+                   
+                    a[k, i] = Math.Pow(data.x_value[k], i);
+                    
+                }
+            }
+
+            for (int k = 0; k < n - 1; k++)
+            {
+                for (int i = k + 1; i < n; i++)
+                {
+                    for (int j = k + 1; j < n; j++)
+                    {
+                        a[i, j] = a[i, j] - a[k, j] * (a[i, k] / a[k, k]);
+                    }
+                    y[i] = y[i] - y[k] * a[i, k] / a[k, k];
+                }
+            }
+
+            for (int k = n - 1; k >= 0; k--)
+            {
+                s = 0;
+                for (int j = k + 1; j < n; j++)
+                    s = s + a[k, j] * x[j];
+                x[k] = (y[k] - s) / a[k, k];
+                
+            }
+
+            return x;
+        }
+
+        public double value_y_gauss(double i) //вводит a в уравнение 
+        { 
+            double summy = 0;
+            for (int j = 0; j <= data.x_value.Length - 1; j++)
+            {
+                summy += graph_gauss()[j] * Math.Pow(i, j);
+            }
+            return summy;
+        }
+    }   
+}
